3c88ff6bfea1911e466f8c24dd20bd5daf866d15
Ben Watson <benwatson528@gmail.com>
2025-07-06 17:36:30 +0100

    Day 7 part 2

--- main/day07/recursive_circus.py
+++ main/day07/recursive_circus.py
@@ -1,2 +1,47 @@
-def solve_p1(nodes, weights) -> str:
-    return list(set(nodes.keys()).difference(set(x for xs in nodes.values() for x in xs)))[0]
+from dataclasses import dataclass
+from typing import List
+
+
+def solve_p1(discs, _) -> str:
+    return list(set(discs.keys()).difference(set(x for xs in discs.values() for x in xs)))[0]
+
+
+# def solve_p2(discs, weights) -> int:
+#     root_name = solve_p1(discs, weights)
+#     root_node = Node(root_name, weights[root_name], children=[])
+#     build_tree(root_node, discs, weights)
+#     current = root_node
+#     while current.children:
+#         summed = {child.name: sum_subtree(child, weights) for child in current.children}
+#         c = Counter(summed.values())
+#         odd_one_out = list(c.keys())[list(c.values()).index(1)]
+#         target = list(set(summed.values()) - {odd_one_out})[0]
+#         odd_key_out = list(summed.keys())[list(summed.values()).index(odd_one_out)]
+#         current = get_child_by_name(current, odd_key_out)
+#         print()
+#     return weights[odd_key_out] - abs(odd_one_out - target)
+
+
+def build_tree(current, discs, weights):
+    for child_name in discs[current.name]:
+        child = Node(child_name, weights[child_name], [])
+        current.children.append(child)
+        build_tree(child, discs, weights)
+
+
+def get_child_by_name(current, child_name):
+    for child in current.children:
+        if child.name == child_name:
+            return child
+    return None
+
+
+sum_subtree = lambda current, weights: weights[current.name] + sum(
+    sum_subtree(child, weights) for child in current.children)
+
+
+@dataclass(frozen=True)
+class Node:
+    name: str
+    weight: int
+    children: List
--- tests/day07/test_recursive_circus.py
+++ tests/day07/test_recursive_circus.py
@@ -13,12 +13,21 @@ def test_p1_real():
     assert solve_p1(*read_input("data/input.txt")) == "dgoocsw"
 
 
+# found answer through manual exploration with code
+# def test_p2_simple():
+#     assert solve_p2(*read_input("data/test_input.txt")) == 60
+#
+#
+# def test_p2_real():
+#     assert solve_p2(*read_input("data/input.txt")) == 1275
+
+
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         nodes = defaultdict(list)
         weights = {}
         for l in f.read().splitlines():
             name = l.split(" ")[0]
-            weights[name] = l.split("(")[1].split(")")[0]
+            weights[name] = int(l.split("(")[1].split(")")[0])
             nodes[name] = l.split(" -> ")[1].split(", ") if " -> " in l else []
     return nodes, weights
